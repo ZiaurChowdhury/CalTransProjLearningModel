c88aec8c247bb28b179aba5b42b6ef8ee886d4ab
RuitaoWu <ruitaowu0@gmail.com>
2022-04-21 15:46:46 -0700

    google charts can not read json file from flask server side

--- static/map.js
+++ static/map.js
@@ -49,29 +49,29 @@ function initMap() {
         infoWindow.open(marker.getMap(),marker);
   }
 /***** */
-function testChart(marker) {
-  var options = {region: 'US', resolution: 'provinces'};
-        var dimension = "population";
-        $.ajax({
-          url: "viewcount.php",
-          dataType: "JSON"
-        }).done(function(data) {
-                var statesArray = [["State",dimension]];
-                $.each(data.states, function() {
-                    var stateitem = [this.abbrev, this[dimension]];
-                    statesArray.push(stateitem);
-                });
-          var statesData = google.visualization.arrayToDataTable(statesArray);
-          let node = document.createElement('div'), info = new google.maps.InfoWindow();
-          var chart = new google.visualization.GeoChart(document.getElementById(node));
-          chart.draw(statesData, options);
-          info.setContent(node);
-          info.open(marker.getMap(),marker);
-          $("h3").append(" Sorted by  "+dimension);
-        });
-}
+// function testChart(marker) {
+//   var options = {region: 'US', resolution: 'provinces'};
+//         var dimension = "population";
+//         $.ajax({
+//           url: "viewcount.php",
+//           dataType: "JSON"
+//         }).done(function(data) {
+//                 var statesArray = [["State",dimension]];
+//                 $.each(data.states, function() {
+//                     var stateitem = [this.abbrev, this[dimension]];
+//                     statesArray.push(stateitem);
+//                 });
+//           var statesData = google.visualization.arrayToDataTable(statesArray);
+//           let node = document.createElement('div'), info = new google.maps.InfoWindow();
+//           var chart = new google.visualization.GeoChart(document.getElementById(node));
+//           chart.draw(statesData, options);
+//           info.setContent(node);
+//           info.open(marker.getMap(),marker);
+//           $("h3").append(" Sorted by  "+dimension);
+//         });
+// }
 /***** */    
 google.load('visualization', '1.0', {'packages':['corechart']});
 // google.setOnLoadCallback(drawChart);
-google.setOnLoadCallback(testChart);
+google.setOnLoadCallback(drawChart);
 
