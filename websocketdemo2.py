2981ccd9f294b50205ca09c4d57d1c1d9969596d
Xin Gao <81401137+honeymustard5999@users.noreply.github.com>
2022-04-21 23:38:05 +0800

    Add files via upload

--- websocketdemo2.py
+++ websocketdemo2.py
@@ -15,62 +15,44 @@ thread_lock = Lock()
 # 后台线程 产生数据，即刻推送至前端
 def background_thread():
     count = 0
-    file = open("fake.json", 'r', encoding='utf-8')
+    file = open("result.json", 'r', encoding='utf-8')
     content = file.readlines()
 
     for i in content:
         socketio.sleep(2)
-        lists = eval(i)
+        dicts = eval(i)
         truck = 0
-        suv = 0
-        bus = 0
+        sedan = 0
         van = 0
         pickup = 0
-        sedan = 0
-
-        for j in lists['preds']:
+        suv = 0
+        bus = 0
+        for j in dicts['preds']:
             if j['label'] == 'truck':
                 truck += 1
-            elif j['label'] == 'suv':
-                suv += 1
-            elif j['label'] == 'bus':
-                bus += 1
-            elif j['label'] == 'van':
-                van += 1
             elif j['label'] == 'pickup':
                 pickup += 1
             elif j['label'] == 'sedan':
                 sedan += 1
-        lists['truck'] = truck
-        lists['suv'] = suv
-        lists['bus'] = bus
-        lists['van'] = van
-        lists['pickup'] = pickup
-        lists['sedan'] = sedan
-        res = json.dumps(lists)
+            elif j['label'] == 'bus':
+                bus += 1
+            elif j['label'] == 'suv':
+                suv += 1
+            elif j['label'] == 'van':
+                van += 1
+
+        dicts['truck'] = truck
+        dicts['pickup'] = pickup
+        dicts['sedan'] = sedan
+        dicts['suv'] = suv
+        dicts['van'] = van
+        dicts['bus'] = bus
+        res = json.dumps(dicts)
+        print(type(res))
         socketio.emit('server_response',
                       {'data': res, 'count': count},
                       namespace='/test')
-    # for i in content:
-    #     res = json.dumps(i)
-    #     socketio.sleep(2)
-    #     count += 1
-    #     t = time.strftime('%M:%S', time.localtime())
-    #     # 获取系统时间（只取分:秒）
-    #     socketio.emit('server_response',
-    #                   {'data': res, 'count': count},
-    #                   namespace='/test')
-    #     # 注意：这里不需要客户端连接的上下文，默认 broadcast = True
-    # for j in content:
-    #     res1 = json.dumps(j)
-    #     socketio.sleep(2)
-    #     count += 1
-    #     t = time.strftime('%M:%S', time.localtime())
-    #     # 获取系统时间（只取分:秒）
-    #     socketio.emit('server_response',
-    #                   {'data': res1},
-    #                   namespace='/test')
-    #     # 注意：这里不需要客户端连接的上下文，，默认 broadcast = True
+
 
 
 @app.route('/')
