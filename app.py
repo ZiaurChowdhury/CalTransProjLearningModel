dcff4c734402b24c43c7b17c5b2c3518050732e2
gvelasquez1548 <79240464+gvelasquez1548@users.noreply.github.com>
2022-04-20 17:10:18 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-# from mysql.connector import MySQLConnection, Error
-# from config import read_db_config
 from flask import Flask, render_template, Response
 import objRealTimeDectector as obj
-from config import read_db_config
-
 #flask
 app = Flask(__name__)
 #camera
@@ -19,20 +15,3 @@ def video_feed():
     return Response(obj.videoDetector(),mimetype="multipart/x-mixed-replace; boundary=frame")
 if __name__ == "__main__":
     app.run(debug=True)
-
-# def connect():
-#     db_config = read_db_config()
-#     conn = None
-#     try:
-#         print('Connecting to MySQL database')
-#         conn = MySQLConnection(**db_config)
-
-#         if conn.is_connected():
-#             print('Connection established')
-#         else:
-#             print('Connection failed')
-    
-#     except Error as error:
-#         print(error)
-
-#     return conn
